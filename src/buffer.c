b82475da427618637e3520c86d4ee1b12a85acf5
azimut <azimut.github@protonmail.com>
2023-01-18 20:02:37 -0300

    fix: add last element, minor use uint

--- src/buffer.c
+++ src/buffer.c
@@ -1,7 +1,6 @@
 #include "buffer.h"
 #include <assert.h>
 #include <math.h>
-#include <stdlib.h>
 #include <time.h>
 
 static unsigned char min(const unsigned char a, const unsigned char b) {
@@ -16,16 +15,16 @@ static unsigned char clamp(const unsigned char velocity) {
   return max(min(velocity, MIDI_MAX), MIDI_MIN);
 }
 
-static unsigned char randomize(const unsigned char velocity) {
+static unsigned char randomize(const char velocity) {
   return clamp(velocity + (random() % 10) - 5);
 }
 
 static unsigned char avg(buffer buffer) {
   assert(buffer.size > 0);
-  unsigned char result = 0;
+  uint result = 0;
   for (int i = 0; i < buffer.size; ++i)
     result += buffer.queue[i];
-  return (unsigned char)round(((result * 1.0) / buffer.size));
+  return clamp((unsigned char)round(((result * 1.0) / buffer.size)));
 }
 
 buffer *buffer_new(void) {
@@ -37,18 +36,19 @@ unsigned char buffer_get(buffer buffer) {
   return (buffer.size == 0) ? randomize(MIDI_DEFAULT) : randomize(avg(buffer));
 }
 
-void buffer_expire(buffer *buffer, const unsigned int now) {
+void buffer_expire(buffer *buffer, const uint now) {
   if ((now - buffer->last_seen) > BUFFER_LIFE)
     buffer->size = 0;
 }
 
-void buffer_add(buffer *buffer, const unsigned char velocity,
-                const unsigned int now) {
+void buffer_add(buffer *buffer, const unsigned char velocity, const uint now) {
   buffer->last_seen = now;
-  if (buffer->size == BUFFER_SIZE)
-    for (int i = 0; i < BUFFER_SIZE - 1; ++i)
+  if (buffer->size == BUFFER_SIZE) {
+    for (int i = 0; i < BUFFER_SIZE - 1; ++i) {
       buffer->queue[i] = buffer->queue[i + 1];
-  else {
+    }
+    buffer->queue[BUFFER_SIZE - 1] = velocity;
+  } else {
     buffer->queue[buffer->size] = velocity;
     buffer->size++;
   }
--- src/buffer.h
+++ src/buffer.h
@@ -1,6 +1,8 @@
 #ifndef BUFFER_H
 #define BUFFER_H
 
+#include <stdlib.h>
+
 #define BUFFER_SIZE 10
 #define BUFFER_LIFE 2
 #define MIDI_DEFAULT 40
@@ -10,13 +12,12 @@
 typedef struct {
   unsigned char queue[BUFFER_SIZE];
   int size;
-  unsigned int last_seen;
+  uint last_seen;
 } buffer;
 
 buffer *buffer_new(void);
 unsigned char buffer_get(buffer buffer);
-void buffer_add(buffer *buffer, const unsigned char velocity,
-                const unsigned int now);
-void buffer_expire(buffer *buffer, const unsigned int now);
+void buffer_add(buffer *buffer, const unsigned char velocity, const uint now);
+void buffer_expire(buffer *buffer, const uint now);
 
 #endif /* BUFFER_H */
