06421afbe12c34ba8d3d38adcb29ccb48c4ba4c2
azimut <azimut.github@protonmail.com>
2023-01-29 06:13:43 -0300

    main: minor refactor

--- src/main.c
+++ src/main.c
@@ -5,52 +5,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MIDI_NAME "midihack"
-#define MIDI_NOTE_TO_REPLACE 127
-
-static snd_seq_t *seq_handle;
+static snd_seq_t *seq;
 static int in_port;
 static int out_port;
 
-void midi_open(void) {
+void midi_start(void) {
   printf("starting...");
-  if ((snd_seq_open(&seq_handle, "default", SND_SEQ_OPEN_DUPLEX, 0)) < 0) {
-    fprintf(stderr, "error: midi_open:snd_seq_open\n");
+  int err;
+  if ((err = snd_seq_open(&seq, "default", SND_SEQ_OPEN_DUPLEX, 0)) < 0) {
+    fprintf(stderr, "ERROR: open = %i (%s)\n", err, snd_strerror(err));
     exit(1);
   }
-  if ((snd_seq_set_client_name(seq_handle, MIDI_NAME)) < 0) {
-    fprintf(stderr, "error: midi_open:snd_seq_set_client_name\n");
+  if ((err = snd_seq_set_client_name(seq, "midihack")) < 0) {
+    fprintf(stderr, "ERROR: name = %i (%s)\n", err, snd_strerror(err));
     exit(1);
   }
   if ((in_port = snd_seq_create_simple_port(
-           seq_handle, "in",
-           SND_SEQ_PORT_CAP_WRITE | SND_SEQ_PORT_CAP_SUBS_WRITE,
+           seq, "in", SND_SEQ_PORT_CAP_WRITE | SND_SEQ_PORT_CAP_SUBS_WRITE,
            SND_SEQ_PORT_TYPE_APPLICATION)) < 0) {
-    fprintf(stderr, "error: midi_open:snd_seq_create_simple_port in_port\n");
+    fprintf(stderr, "ERROR: in_port = %i (%s)\n", in_port,
+            snd_strerror(in_port));
     exit(1);
   }
   if ((out_port = snd_seq_create_simple_port(
-           seq_handle, "out",
-           SND_SEQ_PORT_CAP_READ | SND_SEQ_PORT_CAP_SUBS_READ,
+           seq, "out", SND_SEQ_PORT_CAP_READ | SND_SEQ_PORT_CAP_SUBS_READ,
            SND_SEQ_PORT_TYPE_APPLICATION)) < 0) {
-    fprintf(stderr, "error: midi_open:snd_seq_create_simple_port out_port\n");
+    fprintf(stderr, "ERROR: out_port = %i (%s)\n", out_port,
+            snd_strerror(out_port));
     exit(1);
   }
   printf("done!\n");
 }
 
+void midi_send(snd_seq_event_t *ev) {
+  int err;
+  snd_seq_ev_set_source(ev, out_port);
+  snd_seq_ev_set_subs(ev);
+  snd_seq_ev_set_direct(ev);
+  if ((err = snd_seq_event_output(seq, ev)) < 0) {
+    printf("written = %i (%s)\n", err, snd_strerror(err));
+  }
+  if ((err = snd_seq_drain_output(seq)) < 0) {
+    printf("drain = %i (%s)\n", err, snd_strerror(err));
+  }
+}
+
 int main(void) {
-  midi_open();
+  midi_start();
   printf("listening...\n");
   snd_seq_event_t *ev = NULL;
   Buffer buf = buffer_new();
-  int err;
-  while (snd_seq_event_input(seq_handle, &ev) >= 0) {
+  const int midi_max = 127;
+
+  while (snd_seq_event_input(seq, &ev) >= 0) {
 
     buffer_expire(&buf, ev->time.tick);
 
     if (ev->type == SND_SEQ_EVENT_NOTEON) {
-      if (ev->data.note.velocity == MIDI_NOTE_TO_REPLACE) {
+      if (ev->data.note.velocity == midi_max) {
         ev->data.note.velocity = buffer_get(buf);
         printf("replaced note `%d` velocity with `%d`\n", ev->data.note.note,
                ev->data.note.velocity);
@@ -59,16 +71,8 @@ int main(void) {
       }
     }
 
-    snd_seq_ev_set_source(ev, out_port);
-    snd_seq_ev_set_subs(ev);
-    snd_seq_ev_set_direct(ev);
-
-    if ((err = snd_seq_event_output(seq_handle, ev)) < 0) {
-      printf("written = %i (%s)\n", err, snd_strerror(err));
-    }
-    if ((err = snd_seq_drain_output(seq_handle)) < 0) {
-      printf("drain = %i (%s)\n", err, snd_strerror(err));
-    }
+    midi_send(ev);
   }
+
   return -1;
 }
